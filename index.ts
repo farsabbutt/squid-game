d7e9eb80139b9681b66aa2a8609f4c975884b787
Farsab <farsabb@gmail.com>
2025-01-14 04:47:52 +0100

    finish game implementation

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 const canvas: HTMLCanvasElement = document.getElementById('game')
 const btnMove: HTMLButtonElement = document.getElementById('move')
 const btnStop: HTMLButtonElement = document.getElementById('stop')
+const btnPlay: HTMLButtonElement = document.getElementById('play')
+const btnReset: HTMLButtonElement = document.getElementById('reset')
 
 var soundGameOver: HTMLAudioElement = document.getElementById('game-over');
 var soundRedLightGreenLight: HTMLAudioElement = document.getElementById('red-light-green-light');
+const imageSoldier = document.getElementById("soldier");
+const imageDoll = document.getElementById("doll");
+const imagePlayer = document.getElementById("player");
 
 const ctx = canvas.getContext("2d")
 const canvasWidth = canvas.width
@@ -25,24 +30,15 @@ btnStop.addEventListener('click', () => {
     arePlayersMoving = false
 })
 
+btnPlay.addEventListener('click', () => {
+    btnPlay.hidden = true
+    btnReset.hidden = true
+    loop()
+})
 
-// Create an image element
-const img = new Image();
-
-// Use a placeholder image since we can't load external images
-img.src = 'https://www.pngplay.com/wp-content/uploads/13/Squid-Game-Soldier-Triangle-Cartoon-PNG.png';
-
-img.onload = function() {
-    console.log('drawing....')
-    // Draw the image at position (50,50)
-    console.log(img)
-    ctx.drawImage(img, 100, 100, 100, 100);
-
-    // You can also specify width and height:
-    // ctx.drawImage(img, 50, 50, 100, 100);
-};
-
-
+btnReset.addEventListener('click', () => {
+    location.reload()
+})
 
 function drawPlayer(x, y, fill) {
     ctx.beginPath();
@@ -50,6 +46,7 @@ function drawPlayer(x, y, fill) {
     ctx.fillStyle = fill;
     ctx.fill();
     ctx.closePath();
+    ctx.drawImage(imagePlayer, x-25, y-100, 50, 100)
 }
 
 function drawLine(from, to) {
@@ -61,23 +58,24 @@ function drawLine(from, to) {
 }
 
 function drawFinishingLine() {
-    drawLine([0, 40], [canvasWidth, 40])
+    drawLine([0, 120], [canvasWidth, 120])
 }
 
 function drawDoll(x, y, colorHex) {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, 20, 0, Math.PI * 2);
     ctx.fillStyle = colorHex;
     ctx.fill();
     ctx.closePath();
+    ctx.drawImage(imageDoll, 354        , 0, 90, 120);
 }
 
 function drawPlayers(count) {
     const playerPosY = (canvasHeight - 30) + playersY * playersDy
-    const playerPosX = 40
+    const playerPosX = 120
 
     for (let i = 0; i < count; i++) {
-        drawPlayer(playerPosX + (i * 40), playerPosY, i < playersEliminated ? '#000000' : '#0095DD')
+        drawPlayer(playerPosX + (i * 100), playerPosY, i < playersEliminated ? '#a81423' : '#98dc58')
     }
 
 }
@@ -88,11 +86,19 @@ function drawPlayerEliminated() {
     ctx.fillText("All Players Eliminated!", canvasWidth/2 - 100, canvasHeight/2);
 }
 
+function drawImages() {
+    ctx.drawImage(imageSoldier, 100, 10, 70, 80);
+    ctx.drawImage(imageSoldier, 200, 10, 70, 80);
+    ctx.drawImage(imageSoldier, 500, 10, 70, 80);
+    ctx.drawImage(imageSoldier, 600, 10, 70, 80);
+}
 
 function drawObjects() {
     drawPlayers(playersCount)
     drawDoll(canvasWidth/2, 20, isDollSinging ? '#98dc58' : '#a81423')
     drawFinishingLine()
+    drawImages()
+
 }
 
 let timeout = null
@@ -145,15 +151,15 @@ function loop() {
     if (playersEliminated === playersCount) {
         isGameOver = true
         soundRedLightGreenLight.pause()
-        alert('LOST!')
+        alert('YOU LOST!')
+        btnReset.hidden = false
     }
 
-    if (playersY ==-5450 && playersEliminated < playersCount) {
+    if (playersY ==-4620 && playersEliminated < playersCount) {
         isGameOver = true
         soundRedLightGreenLight.pause()
-        alert('WON!')
+        alert('YOU WON!')
+        btnReset.hidden = false
     }
 }
-
-loop()
 
